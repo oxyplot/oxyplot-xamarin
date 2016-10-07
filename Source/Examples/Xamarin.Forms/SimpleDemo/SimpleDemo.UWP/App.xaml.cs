b068d1e61c205b6c356ed12114c67fdc0530b4b4
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-07 22:37:08 +0200

    Add plot to SimpleDemo, update UWP app

--- Source/Examples/Xamarin.Forms/SimpleDemo/SimpleDemo.UWP/App.xaml.cs
+++ Source/Examples/Xamarin.Forms/SimpleDemo/SimpleDemo.UWP/App.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace SimpleDemo.UWP
@@ -59,6 +49,7 @@ namespace SimpleDemo.UWP
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 Xamarin.Forms.Forms.Init(e);
+                OxyPlot.Xamarin.Forms.Platform.UWP.PlotViewRenderer.Init();
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
--- Source/Examples/Xamarin.Forms/SimpleDemo/SimpleDemo/App.cs
+++ Source/Examples/Xamarin.Forms/SimpleDemo/SimpleDemo/App.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace SimpleDemo
+﻿namespace SimpleDemo
 {
+    using OxyPlot;
+    using OxyPlot.Axes;
+    using OxyPlot.Series;
+    using OxyPlot.Xamarin.Forms;
+    using Xamarin.Forms;
+
     public class App : Application
     {
         public App()
@@ -15,34 +14,37 @@ namespace SimpleDemo
             var content = new ContentPage
             {
                 Title = "SimpleDemo",
-                Content = new StackLayout
+                Content = new PlotView
                 {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                    Model = new PlotModel
+                    {
+                        Title = "OxyPlot in Xamarin Forms.",
+                        Axes =
+                        {
+                            new CategoryAxis {Position = AxisPosition.Bottom},
+                            new LinearAxis {Position = AxisPosition.Left, MinimumPadding = 0}
+                        },
+                        Series =
+                        {
+                            new ColumnSeries
+                            {
+                                Items =
+                                {
+                                    new ColumnItem {Value = 3},
+                                    new ColumnItem {Value = 14},
+                                    new ColumnItem {Value = 11},
+                                    new ColumnItem {Value = 12},
+                                    new ColumnItem {Value = 7}
+                                }
+                            }
                         }
-                    }
+                    },
+                    VerticalOptions = LayoutOptions.Fill,
+                    HorizontalOptions = LayoutOptions.Fill
                 }
             };
 
-            MainPage = new NavigationPage(content);
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+            this.MainPage = new NavigationPage(content);
         }
     }
 }
