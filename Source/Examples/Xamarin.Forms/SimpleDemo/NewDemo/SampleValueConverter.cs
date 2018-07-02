1b24bf5e9bab3b8b3f5ecbefd367681bfd4f2094
Sami M. Kallio <code.kallio@gmail.com>
2018-07-02 14:44:29 -0400

    Updated to latest Xamarin.Forms.
    Changed project type from Portable to .NET Standard 2.0
    
    Updated PlotView to use Device.RuntimePlatform
    
    Changed the condition to include check for DesignMode. This enables Forms Preview to run without encountering an exception.

---
+++ Source/Examples/Xamarin.Forms/SimpleDemo/NewDemo/SampleValueConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using Xamarin.Forms;
+
+namespace NewDemo
+{
+    public class SampleValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var svm = value as SampleViewModel;
+            if (svm == null) return null;
+            var series = new ColumnSeries();
+
+            foreach(var v in svm.Values)
+            {
+                series.Items.Add(new ColumnItem { Value = v });
+            }
+
+            var items = svm.Values.Select(v => new ColumnItem { Value = v }).ToArray();
+            var model = new PlotModel
+            {
+                Title = "OxyPlot in Xamarin Forms.",
+                Axes =
+                {
+                    new CategoryAxis {Position = AxisPosition.Bottom},
+                    new LinearAxis {Position = AxisPosition.Left, MinimumPadding = 0}
+                }
+            };
+
+            model.Series.Add(series);
+
+            return model;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Examples/Xamarin.Forms/SimpleDemo/NewDemo/SampleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewDemo
+{
+    public class SampleViewModel
+    {
+        public IEnumerable<int> Values => new[]
+        {
+            3, 5, 11, 12, 7
+        };
+    }
+}
