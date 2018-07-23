f3f990a7cbee85d7dcb020d7c2febb82ad1a82fb
Oystein Bjorke <oystein.bjorke@gmail.com>
2018-07-23 08:32:34 +0200

    Update Android projects

--- Source/Examples/Xamarin.Android/ExampleBrowser/Properties/AssemblyInfo.cs
+++ Source/Examples/Xamarin.Android/ExampleBrowser/Properties/AssemblyInfo.cs
@@ -8,7 +8,19 @@ using System.Reflection;
 using Android.App;
 
 [assembly: AssemblyTitle("OxyPlot ExampleBrowser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Example browser for Android")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OxyPlot")]
+[assembly: AssemblyProduct("OxyPlot")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 OxyPlot contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The version numbers are updated by the build script. See ~/appveyor.yml
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
+[assembly: AssemblyFileVersion("0.0.1")]
 
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
