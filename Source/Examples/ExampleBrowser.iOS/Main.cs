f3340aab19bbdc9d15b057ed8f08610e2524a246
Janus Weil <janus@gcc.gnu.org>
2023-01-09 23:02:32 +0100

    ExampleBrowser.iOS: fix a warning
    * 'UIApplication.Main(string[]?, string?, string?)' is obsolete: 'Use the overload with 'Type' instead of 'String' parameters for type safety.'

--- Source/Examples/ExampleBrowser.iOS/Main.cs
+++ Source/Examples/ExampleBrowser.iOS/Main.cs
@@ -15,7 +15,7 @@ namespace ExampleBrowser
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
